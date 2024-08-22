678b2732cb459e72e9c825bfbef200a80864f311
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-08-22 11:30:10 -0400

    Tutorial 1:34:50

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 import { connectToDB } from '@utils/database';
+import User from '@models/user';
 
 console.log({
     clientId: process.env.GOOGLE_ID,
