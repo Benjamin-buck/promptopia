f6be0247642e40944c4833b159af528d5a978dd0
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-08-24 06:50:39 -0400

    Tutorial 2:37:10

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,22 +7,37 @@ import { useRouter } from 'next/navigation';
 import Profile from '@components/profile';
 
 const MyProfile = () => {
-  const handleEdit = () => {
+    const { data: session } = useSession();
+    
+    const [posts, setPosts] = useState([]);
 
-  }
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch(`/api/users/${session?.user.id}/posts`);
+            const data = await response.json();
 
-  const handleDelete = async () => {
+            setPosts(data);
+        }
 
-  }
-  return (
-    <Profile
-        name="My"
-        desc="Welcome to your personalized profile page"
-        data={[]}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
-    />
-  )
+        if(session?.user.id) fetchPosts();
+        }, []);
+
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = async () => {
+
+    }
+    return (
+        <Profile
+            name="My"
+            desc="Welcome to your personalized profile page"
+            data={posts}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+        />
+    )
 }
 
 export default MyProfile
