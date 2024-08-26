02244482f59fdddd9890223f6a39f36427067209
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-08-26 16:07:52 -0400

    Tutorial 2:59:58 - Update post pulls data from URL.

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -6,10 +6,10 @@ export const GET = async (request, { params }) => {
     try {
         await connectToDB();
 
-        const prompts = await Prompt.findById(params.id).populate('creator');
+        const prompt = await Prompt.findById(params.id).populate('creator');
         if(!prompt) return new Response("Prompt not found", { status: 404 });
 
-        return new Response(JSON.stringify(prompts), { status:200 })
+        return new Response(JSON.stringify(prompt), { status:200 })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status:500 })
     }
